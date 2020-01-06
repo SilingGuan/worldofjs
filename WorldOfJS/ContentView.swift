c7c99d3ab2050c4e5bcd61787f0d4c5ec98045de
SilingGuan <47166788+SilingGuan@users.noreply.github.com>
2020-01-06 02:47:08 -0800

    Initial Commit

---
+++ WorldOfJS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorldOfJS
+//
+//  Created by Siling Guan on 1/6/20.
+//  Copyright © 2020 Siling Guan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
